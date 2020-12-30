2514805fa962190eb7113c9a49b93a82f14b30cf
pangwa <pangwa@gmail.com>
2020-12-30 12:38:46 +0800

    connect to chain

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import React from 'react'
 import { Web3ReactProvider, useWeb3React, } from '@web3-react/core'
 import { Web3Provider } from '@ethersproject/providers'
 import { useEagerConnect, useInactiveListener } from './hooks'
+import { Spinner } from './Spiner'
+import { injected } from './connectors'
 
 import './App.css';
 
@@ -24,6 +27,63 @@ function ChainId() {
   )
 }
 
+function ConnectChain(props) {
+  const context = useWeb3React()
+  const { connector, library, chainId, account, activate, deactivate, active, error } = context
+
+  const [activatingConnector, setActivatingConnector] = React.useState()
+  React.useEffect(() => {
+    if (activatingConnector && activatingConnector === connector) {
+      setActivatingConnector(undefined)
+    }
+  }, [activatingConnector, connector])
+
+  const activating = injected === activatingConnector
+  const connected = injected === connector
+  const disabled = !props.triedEager || !!activatingConnector || !!error
+
+  useInactiveListener(!props.triedEager || !!activatingConnector)
+
+  let isDisconnect = !error && chainId
+  const buttonText = isDisconnect ? 'Disconnect' : (activating ? 'Connectting' : 'Connect' )
+
+  return (
+    <button
+      style={{
+        borderColor: activating ? 'orange' : connected ? 'green' : 'unset',
+        cursor: disabled ? 'unset' : 'pointer',
+        position: 'relative',
+      }}
+      className="ConnectButton"
+      disabled={disabled}
+      onClick={() => {
+        if (!isDisconnect) {
+          setActivatingConnector(injected)
+          activate(injected)
+        } else {
+          deactivate()
+        }
+      }}
+    >
+      <div
+        style={{
+          position: 'absolute',
+          top: '0',
+          left: '0',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          color: 'black',
+          margin: '0 0 0 1rem'
+        }}
+      >
+        {activating && <Spinner color={'red'} style={{ height: '50%', marginLeft: '-1rem' }} />}
+      </div>
+      { buttonText }
+    </button>
+  )
+}
+
 function App() {
   const triedEager = useEagerConnect()
 
@@ -31,6 +91,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <h1>Counter Example </h1>
+          <ConnectChain triedEager={triedEager} />
           <ChainId/>
           <p>
             Current value: n/a
